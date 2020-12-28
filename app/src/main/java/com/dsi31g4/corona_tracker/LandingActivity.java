c15320057f17fb970c47a18882788ff4e412ce18
aymenkhazri <aymenkhazri6@gmail.com>
2020-12-29 00:01:50 +0100

    add splashScreen and LandingActivity.java

---
+++ app/src/main/java/com/dsi31g4/corona_tracker/LandingActivity.java
@@ -0,0 +1,29 @@
+package com.dsi31g4.corona_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LandingActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        Button btnStart =findViewById(R.id.btnstart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
