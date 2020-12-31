8d89d7e9292e551638cf2fb10f794d34f0f817e4
aymenkhazri <aymenkhazri6@gmail.com>
2020-12-31 04:39:38 +0100

    add code configuration firebase et code VolunteerFragment.java

--- app/src/main/java/com/dsi31g4/corona_tracker/VolunteerFragment.java
+++ app/src/main/java/com/dsi31g4/corona_tracker/VolunteerFragment.java
@@ -4,17 +4,31 @@ package com.dsi31g4.corona_tracker;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.net.URL;
 
 
 public class VolunteerFragment extends Fragment {
 
     ImageButton btnAllo ;
-
+    Button btnSend ;
+    EditText editText ;
+    String  url="https://corona-tracker-b82d0-default-rtdb.firebaseio.com/path/to/data" ;
+    DatabaseReference ref ;
+ VolunteerMessage message ;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -33,6 +47,26 @@ public class VolunteerFragment extends Fragment {
 
             }
         });
+
+        editText=v.findViewById(R.id.Message) ;
+        btnSend=v.findViewById(R.id.btnSend) ;
+        message=new VolunteerMessage() ;
+
+         ref=FirebaseDatabase.getInstance().getReferenceFromUrl(url);
+
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+          message.setMessage(editText.getText().toString().trim());
+            ref.push().setValue(message);
+
+                Toast.makeText(getActivity().getApplicationContext(),"Your Message has been send",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
 
         return v ;
     }
---
+++ app/src/main/java/com/dsi31g4/corona_tracker/VolunteerMessage.java
@@ -0,0 +1,17 @@
+package com.dsi31g4.corona_tracker;
+
+public class VolunteerMessage {
+
+    private String Message ;
+
+    public VolunteerMessage() {
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+}
