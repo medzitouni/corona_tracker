a46f33b52e6bdc3faa0dfe3dbed16fb681c49173
Maya Elabed <maya.elabed22.05@gmail.com>
2020-12-31 01:23:52 +0100

    added java code button admin

--- app/src/main/java/com/dsi31g4/corona_tracker/VolunteerFragment.java
+++ app/src/main/java/com/dsi31g4/corona_tracker/VolunteerFragment.java
@@ -1,19 +1,40 @@
 package com.dsi31g4.corona_tracker;
 
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ImageButton;
 import androidx.fragment.app.Fragment;
 
 
 public class VolunteerFragment extends Fragment {
 
+    ImageButton btnAllo ;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_volunteer, container, false);
+        View v=inflater.inflate(R.layout.fragment_volunteer, container, false);
+
+        btnAllo=v.findViewById(R.id.btnAllo);
+        btnAllo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri admintelephon = Uri.parse("tel:20327836");
+                Intent intent= new Intent(Intent.ACTION_DIAL,admintelephon);
+                startActivity(intent);
+
+
+            }
+        });
+
+        return v ;
     }
+
 }
